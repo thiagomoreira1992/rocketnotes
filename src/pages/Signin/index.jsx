169a942f8a8966031a031d116f6dbbae0edb8256
Thiago Moreira <thiagomoreira.ifsc@gmail.com>
2023-02-15 17:31:16 -0300

    conectando com a api

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -3,21 +3,32 @@ import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
 import { Link } from 'react-router-dom';
+import { useAuth } from "../../hooks/auth";
+import { useState } from "react";
 
 export function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    console.log(email, password)
+    signIn({ email, password })
+  }
+
   return (
     <Container>
       <Form>
         <h1>Rocketnotes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis</p>
         <h2>Faça seu login</h2>
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
-        <Button title="Entrar"/>
+        <Input placeholder="E-mail" type="text" icon={FiMail} onChange={e => setEmail(e.target.value)} />
+        <Input placeholder="Senha" type="password" icon={FiLock} onChange={e => setPassword(e.target.value)} />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">Criar conta</Link>
       </Form>
-      <Background/>
+      <Background />
     </Container>
   );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
+import { useContext} from 'react'
+
+import { useAuth } from '../hooks/auth';
 
 import { AppRoutes} from './app.routes';
 import { AuthRoutes } from './auth.routes';
 
 export function Routes(){
+const data = useAuth()
+
+console.log(data)
+
     return(
         <BrowserRouter>
-            <AppRoutes/>
+            {/* <AppRoutes/> */}
             <AuthRoutes/>
         </BrowserRouter>
     )
